8f081e361be273807cde3454109929990ae1b24c
Duglianny Olavarria <dugliannydesiree1997@gmail.com>
2022-10-18 19:02:17 -0400

    main

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+
+const { src, dest, watch, series, parallel }= require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer')
+
+function css(done){
+  //compilar sass
+  //pasos: 1- identificar archivo, 2- compilarla, 3- guardar el .css
+
+  src('./src/scss/app.scss')
+    .pipe(sass({outputStyle: 'expanded'}))
+      .pipe(postcss([autoprefixer()]))
+        .pipe(dest('build/css'))
+
+      done()
+}
+
+function dev(){
+
+  watch('src/scss/**/*.scss', css)
+  // watch('./src/scss/app.scss', css)
+}
+
+exports.css = css;
+
+exports.dev = dev;
+
+exports.default = series (css, dev );
+
+//series -
+
+//parallel
